5390d6145bc72ef8670c7a4ef6d20d9ecfa55c2d
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-01-02 19:42:51 +0100

    find_enumeration analisys now will copy the output file merged to the output directory of the analisys

--- tools/analisys/shared_analisys_tools/find_enumerations.sh
+++ tools/analisys/shared_analisys_tools/find_enumerations.sh
@@ -3,10 +3,25 @@
 
 find /magma/ -name '*.dump' -size 0 -print -delete;
 
+OutFile="/tmp/EnumerationFoundTotal.dump"
+touch "$OutFile"
+
 shopt -s globstar
 for possibleDump in /magma/**/*.dump; do # Whitespace-safe and recursive
-    echo "$possibleDump"
+    # echo "$possibleDump"
     ls -lh $possibleDump
+    echo "------------" >> $OutFile
+    cat $possibleDump >> $OutFile
 done
 
-bash
+if test -d /output/; then
+    echo "output directory exist."
+    mv $OutFile /output/
+else
+    echo "output directory not defined."
+    echo "dump at $OutFile"
+    echo "spawning a shell"
+    bash
+fi
+
+
